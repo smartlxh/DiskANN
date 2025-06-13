53e3901293349c5882f9f310033e079dee604714
lixianhai <lixianhai.lxh@alibaba-inc.com>
2025-06-13 17:40:49 +0800

    fix

--- src/pq_factory.h
+++ src/pq_factory.h
@@ -1,9 +1,8 @@
 // pq_factory.h
 #pragma once
 #include "pq_table_base.h"
-#include "rabitq_pq_table.h"
 
-enum class PQType { PQ, RaBitQ, LSQ };
+enum class PQType { PQ, OPQ, RaBitQ, LSQ };
 
 template <typename T>
 class PQFactory {
@@ -11,9 +10,8 @@ class PQFactory {
     static std::unique_ptr<PQTableBase<T>> create_pq_table(PQType type) {
         switch(type) {
         case Type::PQ:
+        case Type::OPQ:
             return std::make_unique<FixedChunkPQTableAdapter<T>>();
-        case Type::RaBitQ:
-            return std::make_unique<RabitQPQTableAdapter<T>>();
         default:
             return nullptr;
         }
