fd1d565e0106dd50731720dc484de55da121dfbe
smartlxh <smartlxh@gmail.com>
2025-06-19 17:28:21 +0800

    fix

--- include/pq_table_base.h
+++ include/pq_table_base.h
@@ -9,25 +9,26 @@ class PQTableBase {
     virtual ~PQTableBase() {};
 
     // 查询预处理（中心化/旋转）
-    virtual void preprocess_query(float* query);
+    virtual void preprocess_query(float* query) = 0;
 
-    virtual void train(size_t n, const float* x);
+    virtual void train(size_t n, const float* x) = 0;
+
 
     // 计算每个chunk的距离表
-    virtual void populate_chunk_distances(const float* query, float* out_dists);
+    virtual void populate_chunk_distances(const float* query, float* out_dists) = 0;
 
     // 获取chunk数量 pq
-    virtual uint64_t get_num_chunks();
+    virtual uint64_t get_num_chunks() = 0;
 
     // 支持OPQ等扩展功能的虚函数
     virtual void apply_rotation(float* vec) const { /* 默认无旋转 */ }
 
     virtual void compute_dists(const uint32_t *ids, const uint64_t n_ids, float *dists_out,
-                               uint8_t *data, uint8_t *pq_coord_scratch, float* pq_dists);
+                               uint8_t *data, uint8_t *pq_coord_scratch, float* pq_dists) = 0;
 
-    virtual uint64_t get_num_points();
+    virtual uint64_t get_num_points() = 0;
 
-    virtual void load_pq_compressed_vectors(const std::string &bin_file, uint8_t* &data);
+    virtual void load_pq_compressed_vectors(const std::string &bin_file, uint8_t* &data) = 0;
 
 #ifdef EXEC_ENV_OLS
     virtual void load_pq_centroid_bin(MemoryMappedFiles &files, const char *pq_table_file, size_t num_chunks) {};
