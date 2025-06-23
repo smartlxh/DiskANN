dc5f680c7860302214aa6614e17470a08d5041c7
smartlxh <smartlxh@gmail.com>
2025-06-23 22:51:39 +0800

    fix

--- src/rabit_quantizer.cpp
+++ src/rabit_quantizer.cpp
@@ -283,7 +283,7 @@ void RabitqQuantizer::compute_codes_core(
 
 void RabitqQuantizer::compute_dists (const uint32_t *ids, const uint64_t n_ids, float *dists_out,
                    uint8_t *data, uint8_t *pq_coord_scratch, float* pq_dists) {
-    memset(dists_out, 0, n_pts * sizeof(float));
+    memset(dists_out, 0, npt * sizeof(float));
     for (size_t i = 0; i < n_ids; i++) {
         uint8_t *code = codes + ids[i] * code_size;
         auto distance = distance_to_code(code);
